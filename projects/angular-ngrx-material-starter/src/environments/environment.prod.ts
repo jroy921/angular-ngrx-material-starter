5d5d1a60815b1637d91acd3e389427bde6219c16
jroy921 <51965547+jroy921@users.noreply.github.com>
2020-02-29 11:10:29 -0500

    Rename app in environments

--- projects/angular-ngrx-material-starter/src/environments/environment.prod.ts
+++ projects/angular-ngrx-material-starter/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 const packageJson = require('../../../../package.json');
 
 export const environment = {
-  appName: 'Angular Ngrx Material Starter',
+  appName: 'ORCA',
   envName: 'PROD',
   production: true,
   test: false,
-  i18nPrefix: '/angular-ngrx-material-starter',
+  i18nPrefix: '/orca',
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
